6f69959b17a8913b2d1514534c173e6955d2a2e7
Alex Peterson <aspeterson.126@gmail.com>
2023-10-31 12:18:32 -0500

    Install router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { dummyData } from './data';
 import { removeNull } from './utils';
+import { Routes, Route } from 'react-router-dom';
 
 import { useState, useEffect } from 'react'
 import Header from './components/Header/Header';
 import SearchResults from './components/SearchResults/SearchResults';
 import AllNews from './components/AllNews/AllNews';
+import SingleStory from './components/SingleStory/SingleStory';
 
 import './App.css';
 
@@ -12,21 +14,25 @@ function App() {
   const [allNews, setAllNews] = useState([])
   const [searchResults, setSearchResults] = useState([])
   const [searchInput, setSearchInput] = useState('')
+  const [singleStory, setSingleStory] = useState('')
 
   useEffect(() => {
     let filteredStories = removeNull(dummyData.articles)
-    console.log(filteredStories)
     setAllNews(filteredStories)
   }, []);
 
+  console.log(singleStory)
   return (
     <div>
       <Header setSearchInput={setSearchInput} setSearchResults={setSearchResults} allNews={allNews}/>
+      <Routes>
       {searchResults.length > 0 ? (
-        <SearchResults searchResults={searchResults} searchInput={searchInput}/>
+         <Route path="/" element={<SearchResults searchResults={searchResults} searchInput={searchInput} setSingleStory={setSingleStory}/>}/>
       ) : (
-        <AllNews allNews={allNews} searchInput={searchInput} />
+        <Route path="/" element={<AllNews allNews={allNews} searchInput={searchInput} setSingleStory={setSingleStory}/>}/> 
       )}
+        <Route path="/:title" element={<SingleStory singleStory={singleStory} allNews={allNews}/>}/> 
+      </Routes>
     </div>
   );
 }
