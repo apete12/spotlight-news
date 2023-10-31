6e865b15731011455116b444751d122c156ca4f6
Alex Peterson <aspeterson.126@gmail.com>
2023-10-30 23:04:02 -0500

    Add header with styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+
+  return (
+    <header className="header">
+        <h1 className='app-title'>News Spotlight</h1>
+    </header>
+  );
+}
+
+export default Header;
